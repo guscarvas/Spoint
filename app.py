2269c128c0473c8fd4c77dc0739afcec64cfb699
guscarvas <guscarvas2010@gmail.com>
2020-12-05 12:04:14 -0300

    td instalado

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import os
 import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='sssdhgclshfsh;shd;jshjhsjhjhsjldchljk'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
+
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
 
 
 @app.route('/',methods=['GET'])  # Root of your web platform
