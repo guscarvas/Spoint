f55d54ffd067514ebb797609db49f9b094181d22
Gustavo Carvas <guscarvas@usp.br>
2020-12-05 21:42:25 -0300

    Beginning Performer Consumer

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='sssdhgclshfsh;shd;jshjhsjhjhsjldchljk'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
 
-db=SQLAlchemy(app)
-bcrypt=Bcrypt(app)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 @app.route('/',methods=['GET'])  # Root of your web platform
@@ -17,28 +17,12 @@ def home():
     return '<h1>Hello World!</h1>'
 
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@app.route('/consumer/', methods=['GET','POST'])
+def all_consumer():
+
+    return jsonify()
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,1 +1,34 @@
 from app import db
+
+class Performer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    cost_per_hour = db.Column(db.Float, nullable=False)
+    profile_pic_url = db.Column(db.String(100), nullable=True)
+    birthday = db.Column(db.DateTime, nullable=False)
+    score = db.Column(db.Float, nullable=False, default=0)
+    search_city = db.Column(db.String(30), nullable=False)
+    search_state = db.Column(db.String(30), nullable=False)
+    # address = do we need this?
+    fiscal_code = db.Column(db.String(20), nullable=False)
+    money = db.Column(db.Float, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+
+class Consumer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    profile_pic_url = db.Column(db.String(50), nullable=False)
+    birthday = db.Column(db.DateTime, nullable=False)
+    score = db.Column(db.Float, nullable=False, default=0)
+    # address = do we need this?
+    fiscal_code = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
