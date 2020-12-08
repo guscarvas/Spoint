fba4369d50eaa9d8b73be09f3d8f52dacfae8920
Gustavo Carvas <guscarvas@usp.br>
2020-12-08 15:00:08 -0300

    Some initial progress

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from models import Performer,Consumer
+
 import os
 import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='sssdhgclshfsh;shd;jshjhsjhjhsjldchljk'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+from models import User, Performer, Customer
+
+db.create_all()
+db.session.commit()
 
 @app.route('/',methods=['GET'])  # Root of your web platform
 def home():
@@ -19,9 +23,29 @@ def home():
 
 
 
+################# USERS
+
+@app.route('/user/',methods=['GET','POST'])
+def users():
+    if request.method == 'POST':
+        email = request.json.get('email')
+        password_hash = bcrypt.generate_password_hash(request.json.get('password')).encode('utf-8')
+        role = request.json.get('role')
+        newuser = User(email=email, password=password_hash, role=role)
+        name = request.json.get('name')
+        if role == 'Performer':
+            newperformer = Performer(email=email, user_id=newuser.id, name=name)
+            newuser.performer_id = newperformer.id
+            db.session.add_all([newuser,newperformer])
+        if role == 'Customer':
+            newcustomer = Customer(email=email, user_id=newuser.id, name=name)
+            newuser.customer_id = newcustomer.id
+            db.session.add_all([newuser, newcustomer])
+        db.session.commit()
+    return jsonify(newuser)
 
 @app.route('/performers/', methods=['GET'])
-def all_consumer():
+def performers():
     performers = Performer.query.all()
     return jsonify(performers)
 
--- models.py
+++ models.py
@@ -1,35 +1,41 @@
 from app import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    performer_id = db.relationship('Performer', backref='User', lazy=True,uselist=False)
+    customer_id = db.relationship('Customer', backref='User', lazy=True,uselist=False)
+
 
 class Performer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    # username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password=db.Column(db.String(200),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    cost_per_hour = db.Column(db.Float, nullable=False)
+    cost_per_hour = db.Column(db.Float, nullable=True)
     profile_pic_url = db.Column(db.String(100), nullable=True)
-    birthday = db.Column(db.DateTime, nullable=False)
+    birthday = db.Column(db.DateTime, nullable=True)
     score = db.Column(db.Float, nullable=False, default=0)
-    search_city = db.Column(db.String(30), nullable=False)
-    search_state = db.Column(db.String(30), nullable=False)
-    # address = do we need this?
-    fiscal_code = db.Column(db.String(20), nullable=False)
+    search_city = db.Column(db.String(30), nullable=True)
+    search_state = db.Column(db.String(30), nullable=True)
+    fiscal_code = db.Column(db.String(20), nullable=True)
     money = db.Column(db.Float, nullable=False, default=0)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
 
-class Consumer(db.Model):
+class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    # username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password=db.Column(db.String(200),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    profile_pic_url = db.Column(db.String(50), nullable=False)
+    profile_pic_url = db.Column(db.String(50), nullable=True)
     birthday = db.Column(db.DateTime, nullable=False)
     score = db.Column(db.Float, nullable=False, default=0)
-    # address = do we need this?
-    fiscal_code = db.Column(db.String(20), nullable=False)
+    fiscal_code = db.Column(db.String(20), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
